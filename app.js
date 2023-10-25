40fce8ffe14c619cc8ecea5f0119bb7419e23fc8
unorji kingsley <unorjikingsley@gmail.com>
2023-10-25 11:55:49 +0100

    Node and express

---
+++ app.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const app = express();
+
+// app.use(express.json());
+
+// port = process.env.PORT || 3000;
+
+// app.listen(port, () => console.log(`Server is listening on PORT ${PORT}...`));
+
+console.log('working')
